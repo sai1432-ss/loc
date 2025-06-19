1e4c6b86c78f37428b4b9d87d51945d6b773b012
sai1432-ss <130210212+sai1432-ss@users.noreply.github.com>
2025-06-19 18:31:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 // src/App.tsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import VerifyLoc from './pages/VerifyLoc';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/verf" element={<VerifyLoc />} />
     </Routes>
   );
 }
